d862fb6fd5d9ba3bc7786373a59f6815ad53e8b0
ByungHo <qudgh4290@gmail.com>
2021-11-15 20:53:51 +0900

    hello world

--- helloWorld/src/com/yedam/java/helloWorld/HelloWorld.java
+++ helloWorld/src/com/yedam/java/helloWorld/HelloWorld.java
@@ -3,8 +3,9 @@ package com.yedam.java.helloWorld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 컴퓨터에 실행을 요청할 명령어
+		/* 주석 */
+		System.out.println("Hello World! 환영합니다");
 	}
 
 }
