aecd9a350481ceecdc7a7bf5b1ad5d8beba93a3f
ByungHo <qudgh4290@gmail.com>
2021-11-29 20:03:20 +0900

    commit

--- chapter06_Review/src/com/yedam/java/library/Book.java
+++ chapter06_Review/src/com/yedam/java/library/Book.java
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-package com.yedam.java.library;
-
-public class Book {
-	//field
-	private int bookNum;
-	private String bookName;
-	private String author;
-	private int rentalCount;
-	private boolean isRental;
-	
-	//constructor
-	public Book(int bookNum, String bookName, String author) {
-		this.bookNum = bookNum;
-		this.bookName = bookName;
-		this.author = author;
-		rentalCount = 0;
-		isRental = false;
-	}
-	
-	//method
-	public int getBookNum() {
-		return bookNum;
-	}
-
-	public String getBookName() {
-		return bookName;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public int getRentalCount() {
-		return rentalCount;
-	}
-
-	public void setRentalCount(int rentalCount) {
-		this.rentalCount = rentalCount;
-	}
-
-	public boolean isRental() {
-		return isRental;
-	}
-
-	public void setRental(boolean isRental) {
-		this.isRental = isRental;
-	}
-	
-	
-}
-=======
 package com.yedam.java.library;
 
 public class Book {
@@ -127,4 +71,3 @@ public class Book {
 	
 	
 }
->>>>>>> branch 'main' of https://github.com/Park-hoho/javaGit.git
